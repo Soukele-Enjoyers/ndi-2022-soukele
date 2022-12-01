1a69432c01a56bebdee33772c17e347f8ad2fbec
Gabin Brizay <gabin.brizay@etu.umontpellier.fr>
2022-12-01 22:54:53 +0100

    FIIIIIIIIIIIIIIIXXXXXXXXXXXXx

--- src/Lib/MotDePasse.php
+++ src/Lib/MotDePasse.php
@@ -9,14 +9,14 @@ class MotDePasse
 
     public static function hacher(string $mdpClair): string
     {
-        $mdpPoivre = hash_hmac("sha256", $mdpClair, MotDePasse::$poivre);
+        $mdpPoivre = hash_hmac("sha256", $mdpClair, static::$poivre);
         $mdpHache = password_hash($mdpPoivre, PASSWORD_DEFAULT);
         return $mdpHache;
     }
 
     public static function verifier(string $mdpClair, string $mdpHache): bool
     {
-        $mdpPoivre = hash_hmac("sha256", $mdpClair, MotDePasse::$poivre);
+        $mdpPoivre = hash_hmac("sha256", $mdpClair, static::$poivre);
         return password_verify($mdpPoivre, $mdpHache);
     }
 
