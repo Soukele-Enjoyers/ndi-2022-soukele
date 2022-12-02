51a6d4838f28e4fb1334c11e5b14e2cd296b2ccf
Gabin Brizay <gabin.brizay@etu.umontpellier.fr>
2022-12-02 01:04:49 +0100

    page accueil

---
+++ src/view/accueil.php
@@ -0,0 +1,25 @@
+<div>
+    <h1>SIDAQuest : C'est quoi ?</h1>
+    <aside>
+        <h3>Votre guide personnel</h3>
+    </aside>
+    <article>
+        <h2>Un Serious Game pour sensibiliser aux problématiques liées à la santé sexuelle</h2>
+        <p>
+            SIDAQuest est un RPG dévoloppé lors de la Nuit de L'Informatique 2022 à l'occasion de la 34<sup>e</sup> journée mondiale de lutte contre le SIDA.
+            Dans ce jeu vous incarnez un jeune qui parcoure les campagnes pour combattre les infections sexuellement transmissibles.
+            Cependant dans ces contrées reculées, votre personnage n'est pas au courant des bonnes pratiques et des traitements qui existent pour se prémunir et guérir ses maladies.
+            Au cours de son voyage, il rencontrera des personnages qui l'instruiront et avec qui il apprendra à se protéger des IST.
+        </p>
+        <p>
+            Toutes les interactions que vous, joueur, aurez vous permettront d'en apprendre plus sur les problématiques liées à la sexualité.
+            Toute information que vous récupérerez dans le jeu rejoindra votre guide personnel sur la gauche de votre écran.
+        </p>
+    </article>
+    <aside>
+        <h3>Annonces promotionnelles</h3>
+        <div>
+            <p>Achetez vos préservatifs : 78% de réduction</p>
+        </div>
+    </aside>
+</div>
