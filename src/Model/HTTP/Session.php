437a2be83a2be83bad5f80bf926acddd8831da4d
Gabin Brizay <gabin.brizay@etu.umontpellier.fr>
2022-12-02 01:15:38 +0100

    Updated Session.php

--- src/Model/HTTP/Session.php
+++ src/Model/HTTP/Session.php
@@ -4,22 +4,19 @@ namespace App\SIDAQuest\Model\HTTP;
 use App\SIDAQuest\Config\Conf;
 use Exception;
 
-class Session
-{
+class Session {
     private static ?Session $instance = null;
 
     /**
      * @throws Exception
      */
-    private function __construct()
-    {
-        if (session_start() === false) {
-            throw new Exception("La session n'a pas réussi à démarrer.");
+    private function __construct() {
+        if (!session_start()) {
+            throw new Exception("Session Error: Unable to start.");
         }
     }
 
-    public static function getInstance(): Session
-    {
+    public static function getInstance() : Session {
         if (is_null(static::$instance)) {
             static::$instance = new Session();
             static::verifierDerniereActivite();
@@ -27,38 +24,32 @@ class Session
         return static::$instance;
     }
 
-    public function contient($name): bool
-    {
+    public function contient($name) : bool {
         return isset($_SESSION[$name]);
     }
 
-    public function enregistrer(string $name, $value): void
-    {
+    public function enregistrer(string $name, $value) : void {
         $_SESSION[$name] = $value;
     }
 
-    public function lire(string $name)
-    {
+    public function lire(string $name) : mixed {
         return $_SESSION[$name];
     }
 
-    public function supprimer($name): void
-    {
+    public function supprimer(string $name) : void {
         unset($_SESSION[$name]);
     }
 
-    public function detruire(): void
-    {
+    public function detruire() : void {
         session_unset();     // unset $_SESSION variable for the run-time
         session_destroy();   // destroy session data in storage
         Cookie::supprimer(session_name()); // deletes the session cookie
         static::$instance = null;
     }
 
-    public static function verifierDerniereActivite()
-    {
+    public static function verifierDerniereActivite() {
         if (isset($_SESSION['derniereActivite']) && time() - $_SESSION['derniereActivite'] > (Conf::getDureeExpiration())) {
-            session_unset();
+            static::detruire();
         }
         $_SESSION['derniereActivite'] = time();
     }
